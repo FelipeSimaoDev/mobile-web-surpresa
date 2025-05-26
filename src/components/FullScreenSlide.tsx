8ec424453e58d98594a95e925426db08dccfa131
Felipe <felipe.simao@sou.inteli.edu.br>
2025-05-26 14:52:57 -0300

    feat: fix padding

--- src/components/FullScreenSlide.tsx
+++ src/components/FullScreenSlide.tsx
@@ -21,13 +21,12 @@ const FullScreenSlide: React.FC<FullScreenSlideProps> = ({
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '100vh',
-        width: '100vw',
+        height: '100%',
+        width: '100%',
         cursor: 'pointer',
         backgroundColor: backgroundColor, // Usa a cor de fundo fornecida ou padrão
         color: textColor,                 // Usa a cor de texto fornecida ou padrão
         textAlign: 'center',              // Centraliza o texto dentro do slide
-        padding: '20px'                   // Adiciona um pouco de padding geral
       }}
       onClick={onScreenClick}
     >
